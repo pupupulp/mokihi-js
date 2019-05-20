9275e9e3a6e12ce7681b3cac37623b8e625a3dbc
Eagan Martin <mece.martinece@gmail.com>
2019-05-20 14:29:42 +0800

    Added initial index

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * * This is an opensource project that aims to provide
+ * * a distribution of ExtJS 6.2.0 GPL along with a collection
+ * * of useful component wrapper functions.
+ *
+ * * Feel free to contribute to this repo.
+ *
+ * ! ExtJS 6.2.0 GPL
+ *
+ * * Sample Usage:
+ * * const Mokihi = require('mokihi-js');
+ */
+
+const stringifyObject = require('stringify-object');
